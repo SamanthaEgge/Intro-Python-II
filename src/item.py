b293a9f84472273cfa74bd2496d869b9682cf1dc
Samantha Egge <samantha.c.egge@gmail.com>
2020-04-09 13:24:37 -0700

    Setting up Items and subclasses.

--- src/item.py
+++ src/item.py
@@ -1,2 +1,21 @@
+class Items:
+  def __init__(self, name, description, value):
+    self.name = name
+    self.description = description
+    self.value = value
 
-# class Items
+class Treasures(Items):
+  def __init__(self, rarity):
+    super().__init__(self, name, description, value)
+    self.rarity = rarity
+
+class Weapons(Items):
+  def __init__(self, wpn_type, dmg):
+    super().__init__(self, name, description, value)
+    self.wpn_type = wpn_type
+    self.dmg = dmg
+
+class Utilities(Items):
+  def __init__(self, utility):
+    super().__init__(self, name, description)
+    self.utility = utiltity
